41cacd435796b1ba23b30d96b0a5d0d5ce5e0db9
Cristian Daniel Cornejo Pereira <cristian.cornejo1@outlook.com>
2021-09-11 12:17:24 -0500

    feat: themes and background image

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,24 @@
 import { createStore } from 'vuex'
 
+
+function initialState() {
+  return {
+    isDarkTheme: true
+  }
+}
+
 export default createStore({
-  state: {
-  },
+  state: initialState,
   mutations: {
+    toogleTheme: state => state.isDarkTheme = !state.isDarkTheme
+  },
+  getters: {
+    isDarkTheme: state => state.isDarkTheme
   },
   actions: {
+    toogleTheme({ commit }) {
+      commit("toogleTheme");
+    }
   },
   modules: {
   }
